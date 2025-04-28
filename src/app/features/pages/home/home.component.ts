d9e5c9aa6cf545d8b3c373f280f9794005a449ab
elivandro <elivandro78@gmail.com>
2025-04-28 11:27:42 -0300

    corrigindo menu

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from "@angular/core";
 import { headerComponent } from "./header/header.component";
 import { menuDropComponent } from "./menuDropDown/menudrop.component";
 import { RouterOutlet } from "@angular/router";
+import { footerComponent } from "./foother/footer.component";
 
 @Component({
     selector:"app-home",
     templateUrl:"./home.component.html",
     styleUrl:"./home.component.scss",
     standalone:true,
-    imports:[headerComponent,menuDropComponent,RouterOutlet]
+    imports:[headerComponent,menuDropComponent,RouterOutlet,footerComponent]
 })
 export class homeComponent{
 }
