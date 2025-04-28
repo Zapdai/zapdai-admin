d5c8e9d32598e74d8d81f506c1323cc661469d96
elivandro <elivandro78@gmail.com>
2025-04-28 11:58:51 -0300

    mimi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path:"",redirectTo:"/home",pathMatch:"full"
+        path:"",redirectTo:"/home/dashboard",pathMatch:"full"
     },
     {
         path:"",loadChildren:()=>import("./features/features.routes").then(rota=>rota.routes)
