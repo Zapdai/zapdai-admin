55300a0b6c666888b1f080d29639fc4d247f7989
diegodr7 <diegoreisdr059@gmail.com>
2025-05-14 18:40:45 -0300

    Ajustando responsividade da tela mobile de checkout 05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,38 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'zapdai';
+  title = 'ZAPDAI';
+  private touchStartY = 0;
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      window.addEventListener('touchstart', this.handleTouchStart);
+      window.addEventListener('touchend', this.handleTouchEnd);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.isBrowser) {
+      window.removeEventListener('touchstart', this.handleTouchStart);
+      window.removeEventListener('touchend', this.handleTouchEnd);
+    }
+  }
+
+  handleTouchStart = (e: TouchEvent) => {
+    this.touchStartY = e.touches[0].clientY;
+  };
+
+  handleTouchEnd = (e: TouchEvent) => {
+    const touchEndY = e.changedTouches[0].clientY;
+    const distance = touchEndY - this.touchStartY;
+
+    if (window.scrollY === 0 && distance > 100) {
+      location.reload();
+    }
+  };
 }
