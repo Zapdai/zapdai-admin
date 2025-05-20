89ba0f1906de2b8ae97e7a4b7451cb9e0ff32ef0
elivandro <elivandro78@gmail.com>
2025-05-20 14:28:03 -0300

    correcao de resposta da api socket

--- src/app/services/pagamentosService/pagamentos.service.ts
+++ src/app/services/pagamentosService/pagamentos.service.ts
@@ -1,39 +1,40 @@
 import { Injectable, OnInit } from "@angular/core";
 import { Client, IMessage, Stomp } from '@stomp/stompjs';
+import { Subject } from "rxjs";
 import SockJS from 'sockjs-client';
 
 @Injectable({
     providedIn: "root"
 })
 
-export class ConfirmPagamentoSocketComponent implements OnInit{
+export class ConfirmPagamentoSocketComponent {
     private stompClient!: Client;
-  
-    ngOnInit(): void {
-         this.socketWeb();
-    }
-    email = "elivandro78@gmail.com"
-
-    socketWeb() {
-       
+    private pagamentoSubject = new Subject<any>();
+    // essa resposta que vamos pegar dentor da api ao chamar o Socket  dessa form  this.socketService.pagamento$.subscribe(data => {const status = data.pagamento?.statusPagoPlano;console.log("Status do pagamento:", status);  })
+    pagamento$ = this.pagamentoSubject.asObservable();
+    socketWeb(email: string) {
+      // canexao com servidor zapdai
         this.stompClient = new Client({
             webSocketFactory: () => new SockJS('https://server.zapdai.com/zapdai'),
             reconnectDelay: 5000,
-            debug: () => {},
+            debug: () => { },
         });
 
         this.stompClient.onConnect = (frame) => {
             console.log("conectado")
-
-            this.stompClient.subscribe(`/topic/pagamentos/${this.email}`, (msg: IMessage) => {
-                 const data = JSON.parse(msg.body);
-                        console.log("Data pagametno "+data.pagamento.statusPagoPlano)
-                        return data;
+            //bucando a resposta da api e retornando para o usuario 
+            this.stompClient.subscribe(`/topic/pagamentos/${email}`, (msg: IMessage) => {
+                const data = JSON.parse(msg.body);
+                this.pagamentoSubject.next(data);
 
             })
-                this.sendMessage()
-          
-            
+            /// buscando usuario no banco de daos
+            this.stompClient.publish({
+                destination: '/app/busca',
+                body: JSON.stringify({ email: 'elivandro78@gmail.com' }),
+            });
+
+
         }
 
         this.stompClient.onStompError = (frame) => {
@@ -42,13 +43,6 @@ export class ConfirmPagamentoSocketComponent implements OnInit{
         };
         this.stompClient.activate();
 
-    }
-
-    sendMessage() {
-        this.stompClient.publish({
-            destination: '/app/busca',
-            body: JSON.stringify({ email: 'elivandro78@gmail.com' }),
-        });
     }
 
 }
