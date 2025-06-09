c4a91f74699d19430dba689f2781cf453022e74f
elivandro <elivandro78@gmail.com>
2025-06-09 12:12:25 -0300

    correcao de itens compra no destaque

--- src/app/shared/component/opcoes-categoria/opcoes-categoria.component.ts
+++ src/app/shared/component/opcoes-categoria/opcoes-categoria.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, input } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-opcoes-categoria',
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './opcoes-categoria.component.html',
   styleUrl: './opcoes-categoria.component.scss'
 })
