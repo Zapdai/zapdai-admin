171c55be9e1e7c6353e79879dc2de13762be6fe6
elivandro <elivandro78@gmail.com>
2025-06-13 12:25:41 -0300

    configurando o renderMode

--- src/app/features/pages/categorias/categoria.service.ts
+++ src/app/features/pages/categorias/categoria.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { ApiCategorias } from "../../../services/apiCategorias/apiCategorias.service";
+import { firstValueFrom } from "rxjs";
 
 @Injectable({
     providedIn:"root"
@@ -8,7 +9,10 @@ export class CategoriaServiceNome{
     constructor(private apiCategosrias:ApiCategorias){
 
     }
-     async getNomesCategorias(): Promise<any[]> {
-    return this.apiCategosrias.findAllCategorias().subscribe((response: any) => response.categorias) as unknown as []
+    
+  async getNomesCategorias(): Promise<string[]> {
+    const response = await firstValueFrom(this.apiCategosrias.findAllCategorias());
+    // Garante que response.categorias seja um array
+    return Array.isArray(response?.categorias) ? response.categorias : [];
   }
 }
