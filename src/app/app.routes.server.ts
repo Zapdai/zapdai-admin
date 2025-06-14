159e88262157c6b268e72edd20811ebb0db04638
elivandro <elivandro78@gmail.com>
2025-06-14 13:16:31 -0300

    resolvendo problema da render

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,11 @@
 import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
-import { CategoriasComponent } from './features/pages/categorias/categorias.component';
 import { CategoriaServiceNome } from './features/pages/categorias/categoria.service';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Server
+   {
+    path: '',
+    renderMode: RenderMode.Client,
   },
   {
     path: 'home/categoria/:nome',
@@ -16,5 +15,9 @@ export const serverRoutes: ServerRoute[] = [
       const ids = await dataService.getNomesCategorias(); 
       return ids.map((categoria: any) => ({ nome:categoria.nome })); 
     },
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
