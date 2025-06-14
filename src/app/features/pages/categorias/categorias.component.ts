f663ab6fdd8c5ad81e2beaa4392a920c5a7861fb
elivandro <elivandro78@gmail.com>
2025-06-14 09:54:37 -0300

    corrindo api de produtos

--- src/app/features/pages/categorias/categorias.component.ts
+++ src/app/features/pages/categorias/categorias.component.ts
@@ -12,48 +12,52 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { apiBuscaUserService } from '../../../services/buscaUser/buscaUser.service';
 import { ApiCategorias } from '../../../services/apiCategorias/apiCategorias.service';
 import { Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-categorias',
-  imports: [headerComponent, OpcoesCategoriaComponent, 
-    CarrosselComponent, MaisPostadosComponent, footerComponent, 
+  imports: [headerComponent, OpcoesCategoriaComponent,
+    CarrosselComponent, MaisPostadosComponent, footerComponent,
     MobileNavbarComponent, MatTabsModule],
   templateUrl: './categorias.component.html',
   styleUrl: './categorias.component.scss'
 })
-export class CategoriasComponent implements OnInit{
-  constructor(private apiCategosrias:ApiCategorias,public router:Router){}
-  categorias:any;
-  produtos:any
+export class CategoriasComponent implements OnInit {
+  constructor(private apiCategosrias: ApiCategorias, public router: Router) { }
+  categorias: any;
+  produtos: any
   ngOnInit(): void {
-   this.getAllCategorias()
-   this.getAllProdutos()
+    this.getAllCategorias()
+    this.getAllProdutos()
   }
-  getAllCategorias(){
-   this.apiCategosrias.findAllCategorias().subscribe((item:any)=>{
-     if(item!==null){
-         this.categorias = item.categorias;
-     }
-   })
+  async getAllCategorias() {
+    try {
+      const response = await await firstValueFrom(this.apiCategosrias.findAllCategorias());
+      this.categorias = response.categorias;
+    } catch (erro) {
+      console.log("Erro ao carregar dados!");
+    }
   }
-   getAllProdutos(){
-   this.apiCategosrias.findAllProdutos().subscribe((item:any)=>{
-     if(item!==null){
-         this.produtos = item;
-     }
-   })
+  async getAllProdutos() {
+    try {
+      const response = await firstValueFrom(this.apiCategosrias.findAllProdutos());
+      this.produtos = response;
+    } catch (error) {
+        console.log("Erro ao carregar dados!");
+
+    }
   }
 
 
 
-  navigate(name:any){
+  navigate(name: any) {
     this.router.navigateByUrl(`/home/categoria/${name}`)
   }
-     navigateDetalhesProdutos(name:any){
+  navigateDetalhesProdutos(name: any) {
     this.router.navigateByUrl(`/detalhes/produto/${name}`)
   }
 
-   async getNomesCategorias(): Promise<any[]> {
+  async getNomesCategorias(): Promise<any[]> {
     return this.categorias;
   }
 }
