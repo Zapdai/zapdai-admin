f46ad54f407becfb9dfe5641bd46f070e09700fb
diegodr7 <diegoreisdr059@gmail.com>
2025-06-18 01:32:34 -0300

    Preparando tela de gerenciamento de produtos

--- src/app/features/pages/admin/produtos/produtosAdmin/produtosAdmin.component.ts
+++ src/app/features/pages/admin/produtos/produtosAdmin/produtosAdmin.component.ts
@@ -1,10 +1,22 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector:"app-produtos",
-    imports:[],
+    imports:[ MatIconModule, ReactiveFormsModule ],
     standalone:true,
     templateUrl:"./produtosAdmin.component.html",
     styleUrl:"./produtosAdmin.component.scss"
 })
-export class ProdutosAdminComponent{}
+export class ProdutosAdminComponent{
+
+    groupSearh = new FormGroup({
+        name: new FormControl("")
+    })
+
+    pegarvalor() {
+        const name = this.groupSearh.get("name")?.value;
+        alert("valor digitado " + name)
+    }
+}
